0f6aff8598754ecd1fc1ab16593bee0590224f79
Edward Pinkston <edward.pinkston@gmail.com>
2022-01-31 20:32:33 -0600

    naive

--- ServiceInfo.py
+++ ServiceInfo.py
@@ -1,16 +1,22 @@
+### IMPORTS ###
 import psutil
+import mysql.connector
 import time
 import datetime
+### END IMPORTS ###
 
 while True:
+    # WE WANT TO FILTER THIS POSSIBLY AND FORMAT IT DIFFERENTLY
     print(datetime.datetime.now())
+    # iterates through every process
     for proc in psutil.process_iter():
         try:
             pr = proc.as_dict()
-            #print(f'{pr["name"]}\t{pr["cpu_percent"]}\t{pr["num_threads"]}\t{" ".join(pr["cmdline"][1:]) if pr["cmdline"] else ""}')
-            print(f'{pr["name"]}\t{pr["cpu_percent"]}\t{" ".join(pr["cmdline"][1:]) if pr["cmdline"] else ""}')
+            # ignores many of the windows processes, as we don't want to send those
+            # possibly other programs we want to whitelist in the future
+            if pr["name"] != 'svchost.exe':
+                print(f'{pr["name"]}\t{pr["cpu_percent"]}\t{pr["memory_percent"]}\t{pr["num_threads"]}\t{pr["username"]}')
         except (OSError, psutil.AccessDenied):
             print(pr.name(), 'ACCESS DENIED')
     print('\n*** Ctrl-C to Exit ***\n\n')
-    print(datetime.datetime.now())
-    time.sleep(30) # Sleep for 10 Mins
+    time.sleep(1) # Sleep for 1 second
