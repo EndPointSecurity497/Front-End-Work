c485db377961000964ef622598c0075f3ba87915
Edward Pinkston <edward.pinkston@gmail.com>
2022-02-07 20:30:38 -0600

    Update ServiceInfo.py

--- ServiceInfo.py
+++ ServiceInfo.py
@@ -3,7 +3,24 @@ import psutil
 import mysql.connector
 import time
 import datetime
+from uuid import getnode as get_mac #import statment for getting mac adress
 ### END IMPORTS ###
+
+def macaddr():
+    # figure out what to take from this to get mac addr
+    sub_dict = {'Name': [],'Memory Percent': [], 'Mac': []}
+
+    while True:
+        print(datetime.datetime.now())
+        for proc in psutil.process_iter():
+            try:
+                pr = proc.as_dict()
+                mac = get_mac() #command to get mac address
+                print(f'{pr["name"]}\t{pr["memory_percent"]}\t{"mac"}')
+            except (OSError, psutil.AccessDenied):
+                print(pr.name(), 'ACCESS DENIED')
+        print('\n*** Ctrl-C to Exit ***\n\n')
+        time.sleep(30) # Sleep for 10 Mins
 
 def dump_csv(pslst, fname):
     f = open(fname, 'w')
@@ -11,19 +28,22 @@ def dump_csv(pslst, fname):
     
     for ps in pslst:
         f.write(ps + '\n')
+    f.close()
 
 def main():
+    sys_mem = psutil.virtual_memory()[0]
+
     while True:
-        # WE WANT TO FILTER THIS POSSIBLY AND FORMAT IT DIFFERENTLY
         timestamp = datetime.datetime.now()
         pslst = []
+        
         # iterates through every process
         for proc in psutil.process_iter():
             try:
                 name = proc.name()
-                machine_id = None
+                machine_id = get_mac()
                 mempct = proc.memory_percent()
-                memabs = None
+                memabs = sys_mem * mempct
                 cpupct = proc.cpu_percent()
                 numthd = proc.num_threads()
                 
@@ -32,18 +52,20 @@ def main():
                 pid = proc.pid
                 ###
                 
+                #### WONT WORK WITHOUT A PID, NEED TO ACCOUNT FOR THIS
                 path = None
                 if pid != 0:
                     path = psutil.Process(pid).exe()
             
-                # ignores many of the windows processes, as we don't want to send those
-                # possibly other programs we want to whitelist in the future
-                if name != 'svchost.exe':
-                    pslst.append(f'{timestamp},{machine_id},{name},{mempct},{memabs},{numthd},{usr},{path},{pid}')
+                    # BUG: IF USER NAMES MALWARE SVCHOST.EXE THIS WILL ALLOW MALWARE TO RUN
+                    # ignores many of the windows processes, as we don't want to send those
+                    # possibly other programs we want to whitelist in the future
+                    if name != 'svchost.exe':
+                        pslst.append(f'{timestamp},{machine_id},{name},{mempct},{cpupct},{memabs},{numthd},{usr},{path},{pid}')
             except (OSError, psutil.AccessDenied):
                 print(proc.name(), 'ACCESS DENIED')
                 
-        dump_csv(pslst, 'foo.txt')
+        dump_csv(pslst, 'foo.csv')
         print('\n*** Ctrl-C to Exit ***\n\n')
         time.sleep(1) # Sleep for 1 second
         
