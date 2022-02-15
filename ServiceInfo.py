a9cc539bde62647043d0a7d32b39633a741e1247
Edward Pinkston <edward.pinkston@gmail.com>
2022-02-14 19:59:27 -0600

    tidied up fields and got unique file names

--- ServiceInfo.py
+++ ServiceInfo.py
@@ -1,12 +1,13 @@
 ### IMPORTS ###
 import psutil
-import mysql.connector
+import socket
 import time
 import datetime
 from uuid import getnode as get_mac #import statment for getting mac adress
 ### END IMPORTS ###
 
 def dump_csv(pslst, fname):
+    # BUG: IF FILE IS ALREADY OPEN THEN THIS FAILS
     f = open(fname, 'w')
     f.write('time, machine_id, ps_name, mempct, cpupct, memabs, numthreads, user, path, pid\n')
     
@@ -20,19 +21,23 @@ def main():
     while True:
         timestamp = datetime.datetime.now()
         pslst = []
-        
+        machine_id = get_mac()
+
         # iterates through every process
         for proc in psutil.process_iter():
             try:
                 name = proc.name()
-                machine_id = get_mac()
                 mempct = proc.memory_percent()
-                memabs = sys_mem * mempct
+                memabs = sys_mem * mempct / 100 / 10**6
                 cpupct = proc.cpu_percent()
                 numthd = proc.num_threads()
                 
                 ### NEEDS ADMIN PRIVILEGES
                 usr = proc.username()
+                
+                usr = usr.replace("NT AUTHORITY\\", "", 1)  # removes "NT AUTHORITY" from the user field
+                usr = usr.replace(socket.gethostname() + "\\", "", 1)  # removes hostname (device name) from before username. 
+
                 pid = proc.pid
                 ###
                 
@@ -45,11 +50,11 @@ def main():
                     # ignores many of the windows processes, as we don't want to send those
                     # possibly other programs we want to whitelist in the future
                     if name != 'svchost.exe':
-                        pslst.append(f'{timestamp},{machine_id},{name},{mempct},{cpupct},{memabs},{numthd},{usr},{path},{pid}')
+                        pslst.append(f'{timestamp},{machine_id},{name},{mempct},{cpupct},{memabs} MB,{numthd},{usr},{path},{pid}')
             except (OSError, psutil.AccessDenied):
                 print(proc.name(), 'ACCESS DENIED')
                 
-        dump_csv(pslst, 'foo.csv')
+        dump_csv(pslst, f"{datetime.datetime.now():%Y-%m-%d_h%Hm%Ms%Sa}"+str(machine_id) + '.csv')
         print('\n*** Ctrl-C to Exit ***\n\n')
         time.sleep(1) # Sleep for 1 second
         
