077e541d0cb4c3241054ba2cf80b7c4e5b97df93
Edward Pinkston <edward.pinkston@gmail.com>
2022-04-25 22:03:22 -0500

    final commit

---
+++ startup.ps1
@@ -0,0 +1,11 @@
+﻿$TargetFile = "$PSScriptRoot\ServiceInfo.exe"
+$ShortcutFile = "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ServiceInfo.lnk"
+$WScriptShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+$Shortcut.TargetPath = $TargetFile
+$Shortcut.WorkingDirectory = $PSScriptRoot
+$Shortcut.Save()
+
+$bytes = [System.IO.File]::ReadAllBytes($ShortcutFile)
+$bytes[0x15] = $bytes[0x15] -bor 0x20 #set byte 21 (0x15) bit 6 (0x20) ON
+[System.IO.File]::WriteAllBytes($ShortcutFile, $bytes)
